8b05c99486476d539f3f85f206225df6fc22abee
Daniel Agapov <danielagapov1@gmail.com>
2021-07-18 08:44:25 -0400

    cleaning up, need migration to embed messages rather than through text.

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ function displayRankings(message) {
                     database = _a.sent();
                     users = database.map(function (t) { return t.username; });
                     riceAmounts = database.map(function (t) { return t.rice; });
-                    header = "Rank **|**  Username: Rice Donated";
+                    header = "Rank **|**  Username: Rice Earned";
                     table = "\n" + header + "\n";
                     underline = "**" + '-'.repeat(header.length + 3) + "**\n";
                     table += underline;
@@ -128,50 +128,32 @@ client.on('message', function (msg) {
         displayRankings(msg);
     }
     else if (msg.content === '~play') {
-        /*msg.channel.send('Here is a list of categories you can play with:\n'
-+('Earth Science:'+ ':earth_americas:\n').padStart(24, ' ')
-+'Current Events:'.padEnd(24, ' ') + ':calendar_spiral:\n'
-+'Physics:'.padEnd(24, ' ') + ':atom:\n'
-+'Chinese Phrases:'.padEnd(24, ' ') + ':man_with_chinese_cap:\n'
-+'Chemistry:'.padEnd(24, ' ') + ':scientist:\n'
-+'Cell biology:'.padEnd(24, ' ') + ':microbe:\n'
-+'Geometry:'.padEnd(24, ' ') + ':triangular_ruler:\n'
-+'English Spelling:'.padEnd(24, ' ') + ':abc:\n'
-+'Biology:'.padEnd(24, ' ') + ':biohazard:').then(sent =>{
-            playID = sent.id;
-            playChannel = sent.channel;
-        }); */
-        //sent.react('🌎', '🗓️', '⚛️', '👲', '🧑‍🔬', '🦠', '📐', '🔤', '☣️');
         createUser(msg.author.tag); //Creates a user in the database, does nothing if player is already in database
-        var categoryInfo = function (categoryName, emoji) { return categoryName + emoji.padStart(100 - categoryName.length, ""); }; // change "" to " " if want to fix this
+        var categoryInfo = function (categoryName, emoji) { return categoryName + emoji.padStart(100 - categoryName.length, " "); }; // change "" to " " if want to fix this
         msg.channel.send('Here is a list of categories you can choose (by reacting) to play through the freerice bot: \n\n' +
-            categoryInfo("Earth Science:", ":earth_americas:") + "\n\n" +
-            categoryInfo("Current Events:", ":calendar_spiral:") + "\n\n" +
-            categoryInfo("Physics:", ":atom:") + "\n\n" +
-            categoryInfo("Chinese Phrases:", ":man_with_chinese_cap:") + "\n\n" +
-            categoryInfo("Chemistry:", ":scientist:") + "\n\n" +
-            categoryInfo("Cell biology:", ":microbe:") + "\n\n" +
-            categoryInfo("Geometry:", ":triangular_ruler:") + "\n\n" +
-            categoryInfo("English Spelling:", ":abc:") + "\n\n" +
-            categoryInfo("Biology:", ":biohazard_sign:")).then(function (sent) {
+            categoryInfo("Mathematics:", ":triangular_ruler:") + "\n\n" +
+            categoryInfo("Sciences:", ":atom:") + "\n\n" +
+            categoryInfo("Geography:", ":earth_americas:") + "\n\n" +
+            categoryInfo("English:", ":abc:")).then(function (sent) {
             playID = sent.id;
             playChannel = sent.channel;
             // these reactions are obtained by searching up the ones above with a \ (forward slash)
             // in front of them i.e. \:calendar_spiral:, need to use these since they're universal 
             // and you can't react with the :(emoji): formatted emojis. 
-            sent.react('🌎');
-            sent.react('🗓️');
-            sent.react('⚛️');
-            sent.react('👲');
-            sent.react('🧑‍🔬');
-            sent.react('🦠');
             sent.react('📐');
+            sent.react('⚛️');
+            sent.react('🌎');
             sent.react('🔤');
-            sent.react('☣️');
         });
     }
     else if (msg.content === '~help') {
-        msg.channel.send("Here's a list of the commands: 1. ~freerice 2. ~play 3. ~rankings");
+        var command = function (commandName) { return commandName.padEnd(15, " "); };
+        var helpTitle = 'Below is a list of available commands';
+        msg.channel.send(helpTitle + " \n" +
+            ("**" + '-'.repeat(helpTitle.length + 6) + "**\n") +
+            ("1. ~" + command('freerice') + "|description of the bot and its purpose \n") +
+            ("2. ~" + command('play') + "|play with the freerice bot by answering questions to earn rice \n") +
+            ("3. ~" + command('rankings') + "|see the current server-wide rankings for users' rice earned")); // decide if (server or Discord)-wide
     }
 });
 function question_category(category) {
@@ -191,33 +173,17 @@ client.on('messageReactionAdd', function (reaction, user) {
     var member = reaction.message.guild.members.cache.get(user.id);
     if (reaction.message.id === playID && user.tag !== 'freerice#4898') {
         switch (name) {
-            case '🌎':
-                //scrapeURL('https://freerice.com/categories/earth-science');
-                question_category('🌎');
-                break;
-            case '🗓️':
-                //scrapeURL('https://freerice.com/categories/currentevents');
+            case '📐':
+                question_category('📐');
                 break;
             case '⚛️':
-                //scrapeURL('https://freerice.com/categories/physics');
+                question_category('⚛️');
                 break;
-            case '👲':
-                //scrapeURL('https://freerice.com/categories/chinesephrases');
-                break;
-            case '🧑‍🔬':
-                //scrapeURL('https://freerice.com/categories/chemistry');
-                break;
-            case '🦠':
-                //scrapeURL('https://freerice.com/categories/cellbiology');
-                break;
-            case '📐':
-                //scrapeURL('https://freerice.com/categories/geometry');
+            case '🌎':
+                question_category('🌎');
                 break;
             case '🔤':
-                //scrapeURL('https://freerice.com/categories/englishspelling');
-                break;
-            case '☣️':
-                //scrapeURL('https://freerice.com/categories/biology');
+                question_category('🔤');
                 break;
         }
     }
--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ async function displayRankings(message){ //Not formatted properly yet
     let users = database.map(t => t.username);
     let riceAmounts = database.map(t => t.rice);
 
-    let header:string = "Rank **|**  Username: Rice Donated";
+    let header:string = "Rank **|**  Username: Rice Earned";
     let table:string = `\n${header}\n`;
     let underline: string = `**${'-'.repeat(header.length + 3)}**\n`;
     table += underline;
@@ -75,55 +75,39 @@ client.on('message', msg => {
         displayRankings(msg);
     }
     else if (msg.content === '~play') {
-        /*msg.channel.send('Here is a list of categories you can play with:\n'
-+('Earth Science:'+ ':earth_americas:\n').padStart(24, ' ')
-+'Current Events:'.padEnd(24, ' ') + ':calendar_spiral:\n'
-+'Physics:'.padEnd(24, ' ') + ':atom:\n'
-+'Chinese Phrases:'.padEnd(24, ' ') + ':man_with_chinese_cap:\n'
-+'Chemistry:'.padEnd(24, ' ') + ':scientist:\n'
-+'Cell biology:'.padEnd(24, ' ') + ':microbe:\n'
-+'Geometry:'.padEnd(24, ' ') + ':triangular_ruler:\n'
-+'English Spelling:'.padEnd(24, ' ') + ':abc:\n'
-+'Biology:'.padEnd(24, ' ') + ':biohazard:').then(sent =>{
-            playID = sent.id;
-            playChannel = sent.channel;
-        }); */
-        //sent.react('🌎', '🗓️', '⚛️', '👲', '🧑‍🔬', '🦠', '📐', '🔤', '☣️');
+
     createUser(msg.author.tag); //Creates a user in the database, does nothing if player is already in database
-    const categoryInfo = (categoryName, emoji) => categoryName+emoji.padStart(100-categoryName.length, "") // change "" to " " if want to fix this
+    const categoryInfo = (categoryName, emoji) => categoryName+emoji.padStart(100-categoryName.length, " ") // change "" to " " if want to fix this
     msg.channel.send
     (
     'Here is a list of categories you can choose (by reacting) to play through the freerice bot: \n\n' +
-    categoryInfo("Earth Science:",":earth_americas:")+  "\n\n" +
-    categoryInfo("Current Events:",":calendar_spiral:")+  "\n\n" +
-    categoryInfo("Physics:",":atom:")+  "\n\n" +
-    categoryInfo("Chinese Phrases:",":man_with_chinese_cap:")+  "\n\n" +
-    categoryInfo("Chemistry:",":scientist:")+  "\n\n" +
-    categoryInfo("Cell biology:",":microbe:")+  "\n\n" +
-    categoryInfo("Geometry:",":triangular_ruler:")+  "\n\n" +
-    categoryInfo("English Spelling:",":abc:")+  "\n\n" +
-    categoryInfo("Biology:",":biohazard_sign:")
+    categoryInfo("Mathematics:",":triangular_ruler:")+  "\n\n" +
+    categoryInfo("Sciences:",":atom:") +  "\n\n" +
+    categoryInfo("Geography:",":earth_americas:")+  "\n\n" +
+    categoryInfo("English:",":abc:")
     ).then(sent =>{
         playID = sent.id;
         playChannel = sent.channel;
-        
+    
         // these reactions are obtained by searching up the ones above with a \ (forward slash)
         // in front of them i.e. \:calendar_spiral:, need to use these since they're universal 
         // and you can't react with the :(emoji): formatted emojis. 
-        sent.react('🌎');
-        sent.react('🗓️');
-        sent.react('⚛️');
-        sent.react('👲');
-        sent.react('🧑‍🔬');
-        sent.react('🦠');
+    
         sent.react('📐');
+        sent.react('⚛️');
+        sent.react('🌎');
         sent.react('🔤');
-        sent.react('☣️');
     });
     }
     
     else if (msg.content === '~help') {
-        msg.channel.send("Here's a list of the commands: 1. ~freerice 2. ~play 3. ~rankings");
+        const command = (commandName:string) => commandName.padEnd(15, " ")
+        let helpTitle:string = 'Below is a list of available commands';
+        msg.channel.send(`${helpTitle} \n` +
+        `**${'-'.repeat(helpTitle.length + 6)}**\n` +
+        `1. ~${command('freerice')}|description of the bot and its purpose \n` +
+        `2. ~${command('play')}|play with the freerice bot by answering questions to earn rice \n` +
+        `3. ~${command('rankings')}|see the current server-wide rankings for users' rice earned`); // decide if (server or Discord)-wide
     }
 });
 
@@ -146,33 +130,17 @@ client.on('messageReactionAdd', (reaction, user) =>{
     let member = reaction.message.guild.members.cache.get(user.id);
     if(reaction.message.id === playID && user.tag!== 'freerice#4898'){
         switch (name){
-            case '🌎':
-                //scrapeURL('https://freerice.com/categories/earth-science');
-                question_category('🌎');
-                break;
-            case '🗓️':
-                //scrapeURL('https://freerice.com/categories/currentevents');
+            case '📐':
+                question_category('📐');
                 break;
             case '⚛️':
-                //scrapeURL('https://freerice.com/categories/physics');
+                question_category('⚛️');
                 break;
-            case '👲':
-                //scrapeURL('https://freerice.com/categories/chinesephrases');
-                break;
-            case '🧑‍🔬':
-                //scrapeURL('https://freerice.com/categories/chemistry');
-                break;
-            case '🦠':
-                //scrapeURL('https://freerice.com/categories/cellbiology');
-                break;
-            case '📐':
-                //scrapeURL('https://freerice.com/categories/geometry');
+            case '🌎':
+                question_category('🌎');
                 break;
             case '🔤':
-                //scrapeURL('https://freerice.com/categories/englishspelling');
-                break;
-            case '☣️':
-                //scrapeURL('https://freerice.com/categories/biology');
+                question_category('🔤');
                 break;
         }
     }
